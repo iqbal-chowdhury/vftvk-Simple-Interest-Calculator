cfb9707dc10cf9f876845b76e6ef7baca22b001c
iqbal-chowdhury <iqbal.hasan.chowdhury@gmail.com>
2022-02-06 02:34:58 +1000

    adjust validation according to the test criteria

--- script.js
+++ script.js
@@ -5,18 +5,29 @@ function compute() {
   var interest = principal * years * (rate / 100);
   var year = new Date().getFullYear() + parseInt(years);
 
-  document.getElementById("result").innerHTML =
-    "If you deposit " +
-    principal +
-    ",<br>at an interest rate of " +
-    rate +
-    "%<br>You will receive an amount of " +
-    interest +
-    ",<br>in the year " +
-    year +
-    "<br>";
+  if (
+    (document.getElementById("principal").value == 0) |
+    (document.getElementById("principal").value < 0)
+  ) {
+    window.alert("Enter a positive number");
+    document.getElementById("principal").value = "";
+    document.getElementById("principal").focus();
 
-    document.getElementById("principal").value = '';
+    document.getElementById("principal").value = "";
+  } else {
+    document.getElementById("result").innerHTML =
+      "If you deposit " +
+      principal +
+      ",<br>at an interest rate of " +
+      rate +
+      "%<br>You will receive an amount of " +
+      interest +
+      ",<br>in the year " +
+      year +
+      "<br>";
+
+      document.getElementById("principal").value = "";
+  }
 }
 
 function updateRate() {
@@ -24,11 +35,13 @@ function updateRate() {
   document.getElementById("rate_val").innerText = rateval;
 }
 
-
 function validatePrincipal() {
-  if (document.getElementById("principal").value == 0 | document.getElementById("principal").value < 0 ) {
+  if (
+    (document.getElementById("principal").value == 0) |
+    (document.getElementById("principal").value < 0)
+  ) {
     window.alert("Enter a positive number");
-    document.getElementById("principal").value = '';
+    document.getElementById("principal").value = "";
     document.getElementById("principal").focus();
   }
 }
