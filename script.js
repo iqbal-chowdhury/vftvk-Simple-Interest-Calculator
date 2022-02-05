64c8c15ba8544e054959b8db41fe0f320f1dd1d5
iqbal-chowdhury <iqbal.hasan.chowdhury@gmail.com>
2022-02-06 03:33:52 +1000

    functionality, validation and styling works

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 function compute() {
+
+  /*
+    defining varialbes based ids of the index.html
+  */  
   var principal = document.getElementById("principal").value;
   var rate = document.getElementById("rate").value;
   var years = document.getElementById("yearsValue").value;
   var interest = principal * years * (rate / 100);
   var year = new Date().getFullYear() + parseInt(years);
 
+  /*
+    performing validation on the 'Amount' text field
+    validation fails if user input either zero or negative values
+  */
   if (
     (document.getElementById("principal").value == 0) |
     (document.getElementById("principal").value < 0)
   ) {
-    window.alert("Enter a positive number");
     document.getElementById("principal").value = "";
-    document.getElementById("principal").focus();
+    document.getElementById("result").innerHTML = "";
 
-    document.getElementById("principal").value = "";
+    window.alert("Enter a positive number");
+    document.getElementById("principal").focus();
   } else {
     document.getElementById("result").innerHTML =
       "If you deposit " +
@@ -25,23 +33,13 @@ function compute() {
       ",<br>in the year " +
       year +
       "<br>";
-
-      document.getElementById("principal").value = "";
   }
 }
 
+/*
+    showing dynamic value of the slide-bar
+*/
 function updateRate() {
   var rateval = document.getElementById("rate").value;
   document.getElementById("rate_val").innerText = rateval;
-}
-
-function validatePrincipal() {
-  if (
-    (document.getElementById("principal").value == 0) |
-    (document.getElementById("principal").value < 0)
-  ) {
-    window.alert("Enter a positive number");
-    document.getElementById("principal").value = "";
-    document.getElementById("principal").focus();
-  }
 }
